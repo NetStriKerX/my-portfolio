a10e0f19be7822eb87e668a072688bbd5fc15946
Net <net@Nets-MacBook-Air.local>
2023-03-09 00:46:14 +0700

    fix(image): fix showing image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
       <div className="w-1/3">netkasawat@gmail.com</div>
       <div className="w-1/3">© Copyright2023 | nnnnnnnn</div>
       <div className="w-1/3 flex justify-end items-center gap-2 pr-28">
-        <img src="../../src/Icon/blogo1.png" alt="" className="h-[12px]" />
-        <img src="../../src/Icon/blogo2.png" alt="" className="h-[12px]" />
+        <img src="../../Icon/blogo1.png" alt="" className="h-[12px]" />
+        <img src="../../Icon/blogo2.png" alt="" className="h-[12px]" />
       </div>
     </footer>
   );
