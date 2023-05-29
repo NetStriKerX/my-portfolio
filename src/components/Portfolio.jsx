36b3de363157ee9e0bd4649695e805c95aa865c8
Net <net@Nets-MacBook-Air.local>
2023-05-29 16:41:34 +0700

    fix merge problem

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -13,64 +13,7 @@ function Portfolio() {
           See More →
         </a>
       </div>
-<<<<<<< HEAD
       <EachProject />
-=======
-      <div className="grid gap-4 grid-cols-3 grid-rows-2 px-36 my-12">
-        <a
-          href="https://netstrikerx.github.io/robofriends/"
-          className="flex justify-center items-center"
-          target="_blank"
-        >
-          <img
-            src="../../img/Robot.png"
-            alt="robofriend"
-            width="auto"
-            height="auto"
-            className="rounded-lg"
-          />
-        </a>
-        <a
-          href="https://netstrikerx.github.io/my-web/"
-          className="flex justify-center items-center"
-          target="_blank"
-        >
-          <img
-            src="../../img/firstport.png"
-            alt="my first web portfolio"
-            width="auto"
-            height="auto"
-            className="rounded-lg"
-          />
-        </a>
-        <a
-          href="https://netstrikerx.github.io/background-generator/"
-          className="flex justify-center items-center"
-          target="_blank"
-        >
-          <img
-            src="../../img/background-generator.png"
-            alt="background generator"
-            width="auto"
-            height="auto"
-            className="rounded-lg"
-          />
-        </a>
-        <a
-          href="https://netstrikerx.github.io/"
-          className="flex justify-center items-center"
-          target="_blank"
-        >
-          <img
-            src="../../img/landing-page.png"
-            alt="landing page"
-            width="auto"
-            height="auto"
-            className="rounded-lg"
-          />
-        </a>
-      </div>
->>>>>>> origin/main
     </>
   );
 }
