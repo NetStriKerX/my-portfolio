a6e70eec2d02821003a172a99b233a4a35d3f2ce
Net <net@Nets-MacBook-Air.local>
2023-06-01 12:04:27 +0700

    edit profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,7 +6,7 @@ function Profile() {
         <h2 className="font-bold text-2xl mt-4">I'm Kasawat Chumnirat.</h2>
         <h2 className="font-bold text-2xl mb-8">a Full-Stack Developer.</h2>
         <p>
-          Frontend developer based in bangkok, Thailand. I am coding with a
+          Full Stack Developer based in bangkok, Thailand. I am coding with a
           clean and beautiful problem solving in mind.
         </p>
         <div className="flex mt-6 mb-8 gap-5">
